4b5b0d8ec74deaf9a249ba830a24ae6df342ce74
Zhasforit <90256522+Zhasforit@users.noreply.github.com>
2022-11-03 16:26:19 +0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import re
+import sys
+
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+
+
+class Main(QDialog):
+    def __init__(self):
+        super(Main, self).__init__()
+        loadUi('uis/main.ui', self)
+
+        self.setWindowTitle('Работа со строками в Python')
+        self.setWindowIcon(QtGui.QIcon('images/icon.png'))
+
+        self.btn_solve.clicked.connect(self.solve)
+        self.btn_clear.clicked.connect(self.clear)
+
+    def solve(self):
+        #self.textEdit_words.clear()
+        text = self.textEdit_text.toPlainText()  # получаем наш текст
+        text2 = self.textEdit_words.toPlainText()
+        vyrez = []
+        for i in range(len(text2)):
+            if text2[i] == " ":
+                vyrez.append(0)
+            if text2[i] == "\n":
+                vyrez.append(1)
+        vyrez.append(1)
+        txt = text.split()
+        txt2 = text2.split()
+        stroka = ""
+        for i in txt2:
+            for j in txt:
+                if i == j:
+                    slovo = i.upper()
+                    txt2[txt2.index(j)] = slovo
+                    break
+        for j in range(len(txt2)):
+            if vyrez[j] == 0:
+                stroka += txt2[j] + " "
+            if vyrez[j] == 1:
+                stroka += txt2[j] + "\n"
+        self.textEdit_words.setPlainText(stroka)
+
+        #for s in txt:
+        #    self.textEdit_words.insertPlainText(s.upper()+" ")
+
+    def clear(self):
+        self.textEdit_text.clear()
+        self.textEdit_words.clear()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# python4.2
+# Python_IZT_2_LAB4.2
+Работа с визуальными тестовыми данными в Python 3 с PyQt5
+
+![Screenshot](screenshot0.png)
